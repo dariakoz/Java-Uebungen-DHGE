f46beaf89f8eaca5100566c61e766d774b4652ec
dariakoz <geisler10@gmx.net>
2024-05-22 13:16:26 +0200

    edit and add a) + b)

--- Aufgabenblatt1/src/App.java
+++ Aufgabenblatt1/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
         // HiWI
         String nameHiwi = "Vinzenz Geisler";
         Uebungsgruppe groupHiWI;
@@ -9,13 +10,33 @@ public class App {
         
         Adresse adresseDHGE = new Adresse(strasseDHGE, plzGera, ortGera);
         Hiwi firstHiwi = new Hiwi(nameHiwi, adresseDHGE, groupHiWI);
+
+        // LB
+        String nameLB = "Phillip Stephan";
+        Uebung uebungLB;
+        String strasseDHGE2 = "Weg der Freundschaft 5";
+        String plzGera2 = "07547";
+        String ortGera2 = "Gera2"; 
+        
+        Adresse adresseDHGE2 = new Adresse(strasseDHGE, plzGera, ortGera);
+        Lehrbeauftragter firstLB = new Lehrbeauftragter(nameLB, adresseDHGE2, uebungLB);
+
+        // Prof
+        String nameProf = "Oliver Seide";
+        Vorlesung vorlesungProf;
+        String strasseDHGE3 = "Weg der Freundschaft 6";
+        String plzGera3 = "07548";
+        String ortGera3 = "Gera3"; 
+        
+        Adresse adresseDHGE3 = new Adresse(strasseDHGE, plzGera, ortGera);
+        Professor firstProf = new Professor(nameProf, adresseDHGE3, vorlesungProf);
     }
 }
 
 class Hiwi {
-    String name;
-    Uebungsgruppe gruppe;
-    Adresse adresse;
+    private String name;
+    private Uebungsgruppe gruppe;
+    private Adresse adresse;
 
     public Hiwi(String name, Adresse adresse, Uebungsgruppe gruppe) {
         this.name = name;
@@ -23,18 +44,65 @@ class Hiwi {
         this.adresse = adresse;
     }
     
-    Uebungsgruppe getGruppe() {
-        return gruppe;
+    public Uebungsgruppe getGruppe() {
+        return this.gruppe;
     }
     
-    String getName() {
-        return name;
+    public String getName() {
+        return this.name;
     }
 
-    Adresse getAdresse() {
-        return adresse;
+    public Adresse getAdresse() {
+        return this.adresse;
     }
 }
+class Lehrbeauftragter {
+    private String name;
+    private Uebung uebung;
+    private Adresse adresse;
+
+    public Lehrbeauftragter(String name, Adresse adresse, Uebung uebung) {
+        this.name = name;
+        this.uebung = uebung;
+        this.adresse = adresse;
+    }
+    
+    public Uebung getUebung() {
+        return this.uebung;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public Adresse getAdresse() {
+        return this.adresse;
+    }
+}
+class Professor {
+    private String name;
+    private Vorlesung vorlesung;
+    private Adresse adresse;
+
+    public Professor(String name, Adresse adresse, Vorlesung vorlesung) {
+        this.name = name;
+        this.vorlesung = vorlesung;
+        this.adresse = adresse;
+    }
+    
+    public Vorlesung getVorlesung() {
+        return this.vorlesung;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public Adresse getAdresse() {
+        return this.adresse;
+    }
+}
+
 class Adresse {
     private String strasse;
     private String plz;
@@ -47,15 +115,15 @@ class Adresse {
     }
 
     public String getStrasse() {
-        return strasse;
+        return this.strasse;
     }
 
-    String getPLZ() {
-        return plz;
+    public String getPLZ() {
+        return this.plz;
     }
 
-    String Wohnort() {
-        return Wohnort;
+    public String Wohnort() {
+        return this.Wohnort;
     }
 }
 
